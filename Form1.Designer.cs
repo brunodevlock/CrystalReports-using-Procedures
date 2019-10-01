7cd8712aec4e456ab1a853fee1d6678284a906ee
mobileMartonis <mobile@martonis.com.br>
2019-10-01 15:55:12 -0300

    Crystal Reports C#

---
+++ Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace CrystalReportsTutorial
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.crystalReportViewer1 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
+            this.Show_data = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // crystalReportViewer1
+            // 
+            this.crystalReportViewer1.ActiveViewIndex = -1;
+            this.crystalReportViewer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.crystalReportViewer1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.crystalReportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.crystalReportViewer1.Location = new System.Drawing.Point(0, 0);
+            this.crystalReportViewer1.Name = "crystalReportViewer1";
+            this.crystalReportViewer1.Size = new System.Drawing.Size(771, 617);
+            this.crystalReportViewer1.TabIndex = 0;
+            this.crystalReportViewer1.Load += new System.EventHandler(this.crystalReportViewer1_Load);
+            // 
+            // Show_data
+            // 
+            this.Show_data.Location = new System.Drawing.Point(268, 189);
+            this.Show_data.Name = "Show_data";
+            this.Show_data.Size = new System.Drawing.Size(98, 38);
+            this.Show_data.TabIndex = 1;
+            this.Show_data.Text = "Show Data";
+            this.Show_data.UseVisualStyleBackColor = true;
+            this.Show_data.Click += new System.EventHandler(this.Show_btn);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(771, 617);
+            this.Controls.Add(this.Show_data);
+            this.Controls.Add(this.crystalReportViewer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer1;
+        private System.Windows.Forms.Button Show_data;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,59 @@
+﻿using CrystalDecisions.CrystalReports.Engine;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using System.Configuration;
+
+namespace CrystalReportsTutorial
+{
+
+    public partial class Form1 : Form
+    {
+
+        SqlConnection connection;
+        SqlCommand command;
+        SqlDataAdapter dAdapter;
+        DataTable dtable;
+
+        string conn_string = System.Configuration.ConfigurationManager.ConnectionStrings["connectionstring"].ConnectionString;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void crystalReportViewer1_Load(object sender, EventArgs e)
+        {
+            //    ReportDocument rdc = new ReportDocument(); //criar o objeto Report
+            //    rdc.Load(@"Reports\CrystalReport1.rpt"); //carregar o report
+            //    crystalReportViewer1.ReportSource = rdc; //Executa o source para abrir o Report
+
+
+
+        }
+
+        private void Show_btn(object sender, EventArgs e)
+        {
+            using (connection = new SqlConnection(conn_string))
+            {
+
+                dAdapter = new SqlDataAdapter("GETALLDATA", connection); // faz conexão com a Procedure indicada pelo nome
+                dAdapter.SelectCommand.CommandType = CommandType.StoredProcedure; //habilita o uso de procedures
+                dtable = new DataTable();
+                dAdapter.Fill(dtable);
+                ReportDocument rdc = new ReportDocument(); //instancia o objeto
+                rdc.Load(@"Reports\CrystalReport1.rpt"); // carrega o Report
+                rdc.SetDataSource(dtable);
+
+                crystalReportViewer1.ReportSource = rdc;
+            }
+        }
+    }
+
+}
